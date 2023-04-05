a7575195f54f4c75c1aa2bb90821c15caa6ee00f
Filip Hric <filip@filiphric.com>
2023-04-05 15:01:03 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import QRCode from 'qrcode'
+import { ref } from 'vue';
+
+const result = ref()
+
+const generateQR = async(text: string) => {
+  try {
+    result.value = await QRCode.toDataURL(text)
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+const qrText = ref()
+
+</script>
+
+<template>
+  <h1>QR code generator</h1>
+  <textarea v-model="qrText" />
+  <button @click="generateQR(qrText)">Generate QR</button>
+  <HelloWorld :img="result" />
+</template>
+<style scoped>
+textarea, button {
+  display: block;
+  margin: 0 auto;
+  margin-top: 10px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+defineProps({
+  img: {
+    type: String
+  }
+})
+
+</script>
+
+<template>
+  
+  <img :src="img" />
+</template>
+
+
