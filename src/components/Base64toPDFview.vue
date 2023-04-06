4f77cd932fb66e7d62d469a7618becc69cc87eb2
Filip Hric <filip@filiphric.com>
2023-04-06 11:51:02 +0200

    add base64 to pdf reader

--- src/components/Base64toPDFview.vue
+++ src/components/Base64toPDFview.vue
@@ -1,4 +1,14 @@
 <template>
-  <textarea ref="input" />
-  <div>Not working yet</div>
+  <textarea v-model="base64Text" ref="input" />
+  <object v-if="base64Text.length" type="application/pdf" :data="'data:application/pdf;base64,' + base64Text"></object>
 </template>
+<script setup lang="ts">
+import { ref } from 'vue'
+const base64Text = ref('')
+</script>
+<style scoped>
+object {
+  width: 600px;
+  height: 400px;
+}
+</style>
